f7f8c6048f85b7a7e36fb5e3b19983fa4be5d833
Lachlan Kerr <lachlankerr96@gmail.com>
2021-12-04 15:02:40 +1100

    Finish day 3

--- src/D03/Solution.java
+++ src/D03/Solution.java
@@ -1,6 +1,7 @@
 package D03;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 import java.util.PriorityQueue;
@@ -9,8 +10,6 @@ import java.util.Queue;
 import Utils.Utilities;
 
 public class Solution {
-    //String gammaBinaryString;
-    //String epsilonBinaryString;
     public int n;
 
     public String getGammaBinaryByList(List<String> input) {
@@ -23,7 +22,7 @@ public class Solution {
                 if (value.charAt(i) == '1') {
                     gammaArray[i]++;
                 }
-                if (gammaArray[i] > numLines / 2) {
+                if (gammaArray[i] >= (double)numLines / 2) {
                     gammaString.setCharAt(i, '1');
                 }
             }
@@ -40,29 +39,6 @@ public class Solution {
     }
 
     public int part1(List<String> input) {
-        /*int n = input.get(0).length();
-        int numLines = input.size();
-        int[] gammaArray = new int[n];
-        StringBuilder gammaString = new StringBuilder(new String(new char[n]).replace('\0', '0'));
-
-        for (String value : input) {
-            for (int i = 0; i < n; i++) {
-                if (value.charAt(i) == '1') {
-                    gammaArray[i]++;
-                }
-                if (gammaArray[i] > numLines / 2) {
-                    gammaString.setCharAt(i, '1');
-                }
-            }
-        }
-        String gammaBinaryString = gammaString.toString();
-        int gamma = Integer.parseInt(gammaString.toString(), 2);
-        for (int i = 0; i < n; i++) {
-            gammaString.setCharAt(i, (char)(gammaString.charAt(i) ^ 1));
-        }
-        String epsilonBinaryString = gammaString.toString();
-        int epsilon = Integer.parseInt(gammaString.toString(), 2);*/
-
         int gamma = Integer.parseInt(getGammaBinaryByList(input), 2);
         int epsilon = Integer.parseInt(getEpsilonBinaryByList(input), 2);
 
@@ -70,35 +46,46 @@ public class Solution {
     }
 
     public int part2() {
-        /*List<String> input = Utilities.getInputAsStringList(this);
+        List<String> input = Utilities.getInputAsStringList(this);
         Queue<String> OGRqueue = new PriorityQueue<String>(input);
         Queue<String> OGRnewQueue = new PriorityQueue<String>();
         
         Queue<String> CSRqueue = new PriorityQueue<String>(input);
         Queue<String> CSRnewQueue = new PriorityQueue<String>();
+        String gammaBinaryString = getGammaBinaryByList(input);
+        String epsilonBinaryString = getEpsilonBinaryByList(input);
+
         int n = input.get(0).length();
         for (int i = 0; i < n; i++) {
-            while (OGRqueue.peek() != null) {
-                String next = OGRqueue.poll();
-                if (next.charAt(i) == gammaBinaryString.charAt(i)) {
-                    OGRnewQueue.add(next);
+            if (OGRqueue.size() > 1) {
+                while (OGRqueue.peek() != null) {
+                    String next = OGRqueue.poll();
+                    if (next.charAt(i) == gammaBinaryString.charAt(i)) {
+                        OGRnewQueue.add(next);
+                    }
                 }
+                OGRqueue = new PriorityQueue<String>(OGRnewQueue);
+                OGRnewQueue = new PriorityQueue<String>();
             }
-            OGRqueue = new PriorityQueue<String>(OGRnewQueue);
-            OGRnewQueue = new PriorityQueue<String>();
-            while (CSRqueue.peek() != null) {
-                String next = CSRqueue.poll();
-                if (next.charAt(i) == epsilonBinaryString.charAt(i)) {
-                    CSRnewQueue.add(next);
+            if (CSRqueue.size() > 1) {
+                while (CSRqueue.peek() != null) {
+                    String next = CSRqueue.poll();
+                    if (next.charAt(i) == epsilonBinaryString.charAt(i)) {
+                        CSRnewQueue.add(next);
+                    }
                 }
+                CSRqueue = new PriorityQueue<String>(CSRnewQueue);
+                CSRnewQueue = new PriorityQueue<String>();
             }
-            CSRqueue = new PriorityQueue<String>(CSRnewQueue);
-            CSRnewQueue = new PriorityQueue<String>();
-            part1() 
+            gammaBinaryString = getGammaBinaryByList(new ArrayList<String>(OGRqueue));
+            epsilonBinaryString = getEpsilonBinaryByList(new ArrayList<String>(CSRqueue));
         }
 
-        */
-        return 0;
+        
+        int OGR = Integer.parseInt(OGRqueue.poll(), 2);
+        int CSR = Integer.parseInt(CSRqueue.poll(), 2);
+        
+        return OGR * CSR;
     }
 
     public static void main(String[] args) {
