b9bbad0dc7499b161f04c34c30972b977311d8d0
Lachlan Kerr <lachlankerr96@gmail.com>
2021-12-07 00:25:34 +1100

    Adjust day 06 but still not working

--- src/D06/Solution.java
+++ src/D06/Solution.java
@@ -1,27 +1,52 @@
 package D06;
 
+import java.math.BigInteger;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import Utils.Utilities;
 
 public class Solution {
-    public int part1or2(int days) {
+    public long part1or2(int days) {
         List<String> input = Utilities.getInputAsStringList(this);
-        List<Integer> fish = Arrays.stream(input.get(0).split(",")).mapToInt(Integer::parseInt).boxed().collect(Collectors.toList());
+        HashMap<Integer, Integer> fishHash = new HashMap<Integer, Integer>();
+        List<Integer> initialFish = Arrays.stream(input.get(0).split(",")).mapToInt(Integer::parseInt).boxed().collect(Collectors.toList());
+
+        for (int fishTimer : initialFish) {
+            if (fishHash.containsKey(fishTimer)) {
+                fishHash.put(fishTimer, fishHash.get(fishTimer) + 1);
+            }
+            else {
+                fishHash.put(fishTimer, 1);
+            }
+        }
+
         for (int i = 0; i < days; i++) {
-            for (int j = 0; j < fish.size(); j++) {
-                if (fish.get(j) == 0) {
-                    fish.add(9); //9 because we have to do the minus
-                    fish.set(j, 6);
-                }
-                else {
-                    fish.set(j, fish.get(j) - 1);
+            HashMap<Integer, Integer> oldFishHash = new HashMap<Integer,Integer>(fishHash);
+            for (int j = 0; j < 9; j++) {
+                if (fishHash.containsKey(j)) {
+                    if (j == 0) {
+                        int seven = 0;
+                        if (oldFishHash.containsKey(7)) {
+                            seven = oldFishHash.get(7);
+                        }
+                        fishHash.put(6, oldFishHash.get(0) + seven);
+                    }
+                    else if (j != 6) {
+                        if (oldFishHash.containsKey(j - 1)) {
+                            fishHash.put(j, oldFishHash.get(j - 1));
+                        }
+                    }
                 }
             }
         }
-        return fish.size();
+        long count = 0;
+        for (int value : fishHash.values()) {
+            count += value;
+        }
+        return count;
     }
 
     public static void main(String[] args) {
