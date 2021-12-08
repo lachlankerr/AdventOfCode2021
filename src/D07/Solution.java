9e845a3ce3b87833b4394fd15211e4d60923b0a8
Lachlan Kerr <lachlankerr96@gmail.com>
2021-12-08 19:23:23 +1100

    Finish day 07

---
+++ src/D07/Solution.java
@@ -0,0 +1,46 @@
+package D07;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import Utils.Utilities;
+
+public class Solution {
+    public int part1() {
+        List<String> input = Utilities.getInputAsStringList(this);
+        int[] positions = Arrays.stream(input.get(0).split(",")).mapToInt(Integer::parseInt).toArray();
+        Arrays.sort(positions);
+        int median = positions[positions.length/2];
+
+        int fuel = 0;
+        for (int position : positions) {
+            fuel += Math.abs(position - median);
+        }
+
+        return fuel;
+    }
+
+    public int part2() {
+        List<String> input = Utilities.getInputAsStringList(this);
+        int[] positions = Arrays.stream(input.get(0).split(",")).mapToInt(Integer::parseInt).toArray();
+        int sum = IntStream.of(positions).sum();
+        int mean = sum / positions.length;
+        
+        int fuel = 0;
+        for (int position : positions) {
+            int n = Math.abs(position - mean);
+            fuel += (n*(n+1))/2;
+        }
+
+        return fuel;
+    }
+
+    public static void main(String[] args) {
+        Solution day07 = new Solution();
+        System.out.println(day07.part1());
+        System.out.println(day07.part2());
+    }
+}
