0290d7b1425026067d1edaa1c2379b46286611de
Lachlan Kerr <lachlankerr96@gmail.com>
2021-12-09 16:30:16 +1100

    Start on day 09 part 1

--- src/D09/Solution.java
+++ src/D09/Solution.java
@@ -1,8 +1,36 @@
 package D09;
 
+import java.util.Arrays;
+import java.util.List;
+
+import Utils.Utilities;
+
 public class Solution {
+    int[][] grid;
+
     public int part1() {
-        return 0;
+        List<String> input = Utilities.getInputAsStringList(this);
+        grid = new int[input.size()][input.get(0).length()];
+
+        for (int i = 0; i < input.size(); i++) {
+            grid[i] = Arrays.stream(input.get(i).split("(?!^)")).mapToInt(Integer::parseInt).toArray();
+        }
+
+        int riskLevels = 0;
+
+        for (int row = 0; row < input.size(); row++ ) {
+            for (int col = 0; col < input.get(0).length(); col++ ) {
+                if (isLowPoint(row, col)) {
+                    riskLevels += grid[row][col] + 1;
+                }
+            }
+        }
+
+        return riskLevels;
+    }
+
+    public boolean isLowPoint(int row, int col) {
+        return true;
     }
 
     public int part2() {
