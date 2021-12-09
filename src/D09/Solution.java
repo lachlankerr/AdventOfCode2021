d5c212876cdcdbfbdc1f3ebeb0d60d6836100f66
Lachlan Kerr <lachlankerr96@gmail.com>
2021-12-09 14:47:46 +1100

    Add setup for day 09

---
+++ src/D09/Solution.java
@@ -0,0 +1,17 @@
+package D09;
+
+public class Solution {
+    public int part1() {
+        return 0;
+    }
+
+    public int part2() {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Solution day09 = new Solution();
+        System.out.println(day09.part1());
+        System.out.println(day09.part2());
+    }
+}
