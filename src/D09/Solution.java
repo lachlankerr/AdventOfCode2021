be8e7b9620c26fe971810a37e18e8fc3567a272b
Lachlan Kerr <lachlankerr96@gmail.com>
2021-12-09 21:32:53 +1100

    Finish day 09

--- src/D09/Solution.java
+++ src/D09/Solution.java
@@ -1,7 +1,14 @@
 package D09;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Objects;
+import java.util.Queue;
+import java.util.Set;
 
 import Utils.Utilities;
 
@@ -9,6 +16,7 @@ public class Solution {
     int[][] grid;
     int rows;
     int cols;
+    List<Point> lowPoints = new ArrayList<Point>();
 
     public int part1() {
         List<String> input = Utilities.getInputAsStringList(this);
@@ -24,8 +32,10 @@ public class Solution {
 
         for (int row = 0; row < rows; row++ ) {
             for (int col = 0; col < cols; col++ ) {
-                if (isLowPoint(row, col)) {
+                Point p = new Point(col, row);
+                if (isLowPoint(p)) {
                     riskLevels += grid[row][col] + 1;
+                    lowPoints.add(p);
                 }
             }
         }
@@ -33,23 +43,86 @@ public class Solution {
         return riskLevels;
     }
 
-    public boolean isLowPoint(int row, int col) {
-        int current = grid[row][col];
-        if (row > 0         && grid[row - 1][col] <= current ) return false; //up
-        if (row < rows - 1  && grid[row + 1][col] <= current ) return false; //down
-        if (col > 0         && grid[row][col - 1] <= current ) return false; //left
-        if (col < cols - 1  && grid[row][col + 1] <= current ) return false; //right
+    public boolean isLowPoint(Point p) {
+        int current = grid[p.y][p.x];
+        
+        // x = col, y = row
+        if (p.y > 0         && grid[p.y - 1][p.x] <= current ) return false; //up
+        if (p.y < rows - 1  && grid[p.y + 1][p.x] <= current ) return false; //down
+        if (p.x > 0         && grid[p.y][p.x - 1] <= current ) return false; //left
+        if (p.x < cols - 1  && grid[p.y][p.x + 1] <= current ) return false; //right
 
         return true;
     }
 
     public int part2() {
-        return 0;
+        List<Integer> basinSizes = new ArrayList<Integer>();
+
+        for (Point lp : lowPoints) {
+            Set<Point> seen = new HashSet<Point>();
+            Queue<Point> queue = new LinkedList<Point>();
+            queue.add(lp);
+            while (queue.size() > 0) {
+                Point current = queue.poll();
+                List<Point> validPoints = getValidPointsAroundPoint(current);
+                for (Point validPoint : validPoints) {
+                    if (!seen.contains(validPoint)) {
+                        seen.add(validPoint);
+                        queue.add(validPoint);
+                    }
+                }
+            }
+            basinSizes.add(seen.size());
+        }
+
+        Collections.sort(basinSizes, Collections.reverseOrder());
+
+        return basinSizes.get(0) * basinSizes.get(1) * basinSizes.get(2);
+    }
+
+    public List<Point> getValidPointsAroundPoint(Point p) {
+        List<Point> validPoints = new ArrayList<Point>();
+        int boundary = 9;
+
+        // x = col, y = row
+        if (p.y > 0         && grid[p.y - 1][p.x] < boundary ) validPoints.add(new Point(p.x, p.y - 1)); //up
+        if (p.y < rows - 1  && grid[p.y + 1][p.x] < boundary ) validPoints.add(new Point(p.x, p.y + 1)); //down
+        if (p.x > 0         && grid[p.y][p.x - 1] < boundary ) validPoints.add(new Point(p.x - 1, p.y)); //left
+        if (p.x < cols - 1  && grid[p.y][p.x + 1] < boundary ) validPoints.add(new Point(p.x + 1, p.y)); //right
+
+        return validPoints;
     }
 
     public static void main(String[] args) {
         Solution day09 = new Solution();
         System.out.println(day09.part1());
         System.out.println(day09.part2());
+    }
+
+    public class Point {
+        public int x;
+        public int y;
+
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj)
+                return true;
+            if (obj == null)
+                return false;
+            if (getClass() != obj.getClass())
+                return false;
+            Point point = (Point) obj;
+            return point.x == x && point.y == y;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(x, y);
+        }
     }
 }
