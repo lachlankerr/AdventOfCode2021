4cd15a868755f7abe420ad62758225d81d2fcf11
Lachlan Kerr <lachlankerr96@gmail.com>
2021-12-10 14:34:04 +1100

    Add setup for day 10

---
+++ src/D10/Solution.java
@@ -0,0 +1,17 @@
+package D10;
+
+public class Solution {
+    public int part1() {
+        return 0;
+    }
+
+    public int part2() {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Solution day10 = new Solution();
+        System.out.println(day10.part1());
+        System.out.println(day10.part2());
+    }
+}
