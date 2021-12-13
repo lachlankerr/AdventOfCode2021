8ab54aa6465f9415e29fdb478dbdfe0b3bd73ba5
Lachlan Kerr <lachlankerr96@gmail.com>
2021-12-13 22:54:47 +1100

    Start on day 12 part 2

--- src/D12/Solution.java
+++ src/D12/Solution.java
@@ -3,6 +3,7 @@ package D12;
 import java.util.AbstractMap;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -31,46 +32,99 @@ public class Solution {
         Node end = graph.getNode("end");
 
         Stack<Tuple<Node, List<Node>>> stack = new Stack<Tuple<Node, List<Node>>>();
-        Set<Node> discovered = new HashSet<Node>();
-        //stack.push(new Tuple<Node, List<Node>>(start, Arrays.asList(start)));
         stack.push(new Tuple<Node, List<Node>>(start, new ArrayList<Node>()));
         while (stack.size() > 0) {
             Tuple<Node, List<Node>> next = stack.pop();
             if (next.x.isBig() || !next.y.contains(next.x)) {
-            //if (!discovered.contains(next.x)) {
                 if (next.x.equals(end)) {
                     paths.add(next.y);
                     continue;
                 }
-                //if (!next.x.isBig() && !next.x.equals(end)) {
-                //    discovered.add(next.x);
-                //}
                 for (Node neighbour : graph.getNeighbours(next.x)) {
                     stack.push(new Tuple<Node, List<Node>>(neighbour, Stream.concat(next.y.stream(), Arrays.asList(next.x).stream()).collect(Collectors.toList())));
                 }
             }
         }
 
-        for (List<Node> path : paths) {
+        /*for (List<Node> path : paths) {
             String pathString = "";
             for (Node node : path) {
                 pathString += node.id + ",";
             }
             System.out.println(pathString + "end");
             //System.out.println(pathString.substring(0, pathString.length() - 1));
-        }
+        }*/
 
 
         return paths.size();
     }
 
     public int part2() {
-        return 0;
+        List<String> input = Input.getAsStringList(this);
+        for (String line : input) {
+            String[] parts = line.split("-");
+            graph.addEdge(parts[0], parts[1]);
+        }
+
+        List<List<Node>> paths = new ArrayList<List<Node>>();
+
+        Node start = graph.getNode("start");
+        Node end = graph.getNode("end");
+
+        Stack<Tuple<Node, List<Node>>> stack = new Stack<Tuple<Node, List<Node>>>();
+        stack.push(new Tuple<Node, List<Node>>(start, new ArrayList<Node>()));
+        while (stack.size() > 0) {
+            Tuple<Node, List<Node>> next = stack.pop();
+            if (next.x.isBig() || part2check(next.y, next.x)) {
+                if (next.x.equals(end)) {
+                    paths.add(next.y);
+                    continue;
+                }
+                for (Node neighbour : graph.getNeighbours(next.x)) {
+                    stack.push(new Tuple<Node, List<Node>>(neighbour, Stream.concat(next.y.stream(), Arrays.asList(next.x).stream()).collect(Collectors.toList())));
+                }
+            }
+        }
+
+
+        return paths.size();
+    }
+
+    public boolean part2check(List<Node> path, Node toAdd) {
+        List<Node> newPath = Stream.concat(path.stream(), Arrays.asList(toAdd).stream()).collect(Collectors.toList());
+
+        HashMap<Node, Integer> counts = new HashMap<Node, Integer>();
+        
+        for (Node node : newPath) {
+            if (!node.isBig() && !node.id.equals("start") && !node.id.equals("end")) {
+                int curr = 0;
+                if (counts.containsKey(node)) {
+                    curr = counts.get(node);
+                }
+                counts.put(node, curr + 1);
+            }
+        }
+
+        int twocounts = 0;
+
+        for (int value : counts.values()) {
+            if (value > 2) {
+                return false;
+            }
+            if (value > 1) {
+                twocounts++;
+            }
+        }
+
+        return twocounts < 2;
     }
 
     public static void main(String[] args) {
         Solution day12 = new Solution();
         System.out.println(day12.part1());
+        System.out.println(day12.part2check(Arrays.asList(day12.new Node("a")), day12.new Node("a")));
+        System.out.println(day12.part2check(Arrays.asList(day12.new Node("a"), day12.new Node("a")), day12.new Node("a")));
+        System.out.println(day12.part2check(Arrays.asList(day12.new Node("a"), day12.new Node("b"), day12.new Node("b")), day12.new Node("a")));
         System.out.println(day12.part2());
     }
 
@@ -111,6 +165,16 @@ public class Solution {
                 }
             }
             return neighbours;
+        }
+
+        public List<Node> getSmallCaves() {
+            List<Node> smallCaves = new ArrayList<Node>();
+            for (Node node : nodes) {
+                if (!node.isBig() && !node.id.equals("start") && !node.id.equals("end")) {
+                    smallCaves.add(node);
+                }
+            }
+            return smallCaves;
         }
     }
 
