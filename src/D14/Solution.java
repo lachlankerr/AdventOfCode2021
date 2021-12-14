ab5dfa35d732c275b5cfaa16adf5eebd9c6c3c85
Lachlan Kerr <lachlankerr96@gmail.com>
2021-12-14 21:37:09 +1100

    Finish day 14

--- src/D14/Solution.java
+++ src/D14/Solution.java
@@ -2,58 +2,91 @@ package D14;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import java.util.Objects;
 
-import Utils.Grid;
 import Utils.Input;
-import Utils.Point;
-import Utils.Tuple;
 
 public class Solution {
-    public int part1() {
+    public long part1or2(int steps) {
         var input = Input.getAsStringList(this);
+
+        // get our polymer template
         String polymerTemplate = input.remove(0);
         input.remove(0); //remove newline
+
+        // construct insertion pairs from input
         var pairInsertions = new ArrayList<PairInsertion>();
         for (String line : input) {
-            String[] parts = line.split(" -> ");
-            PairInsertion pairInsertion = new PairInsertion(parts[0], parts[1]);
+            var parts = line.split(" -> ");
+            var pairInsertion = new PairInsertion(parts[0], parts[1]);
             pairInsertions.add(pairInsertion);
         }
 
-        var map = new HashMap<Pair, Integer>();
+        // initial fill of our pair map
+        var map = new HashMap<Pair, Long>();
         for (int i = 0; i < polymerTemplate.length() - 1; i++) {
             var pair = new Pair(polymerTemplate.substring(i, i + 2));
-            int count = 0;
+            long count = 0;
             if (map.containsKey(pair)) {
                 count = map.get(pair);
             }
             map.put(pair, count + 1);
         }
-        return 0;
-    }
-    
-    public int part2() {
-        return 0;
+
+        // create new pairs from the insertion for our original pairs, for the given amount of steps
+        for (int i = 0; i < steps; i++) {
+            var newMap = new HashMap<Pair, Long>();
+            for (var entrySet : map.entrySet()) {
+                var key = entrySet.getKey();
+                var value = entrySet.getValue();
+                var pairInsertion = pairInsertions.stream().filter(pi -> pi.pair.equals(key)).findFirst().orElse(null);
+                var leftValue = newMap.getOrDefault(pairInsertion.leftPair, 0l) + value;
+                var rightValue = newMap.getOrDefault(pairInsertion.rightPair, 0l) + value;
+                newMap.put(pairInsertion.leftPair, leftValue);
+                newMap.put(pairInsertion.rightPair, rightValue);
+            }
+            map = newMap;
+        }
+
+        // construct frequency map
+        var freqMap = new HashMap<Character, Long>();
+        for (var entrySet : map.entrySet()) {
+            var key = entrySet.getKey();
+            var value = entrySet.getValue();
+            freqMap.put(key.left, freqMap.getOrDefault(key.left, 0l) + value);
+            freqMap.put(key.right, freqMap.getOrDefault(key.right, 0l) + value);
+        }
+
+        // adjust frequencies due to pairs
+        long smallest = Long.MAX_VALUE;
+        long largest = 0;
+        for (var count : freqMap.values()) {
+            if (count % 2 == 1)     count = (count + 1) / 2;
+            else                    count = count / 2;
+            if (count > largest)    largest = count;
+            if (count < smallest)   smallest = count;
+        }
+
+        return largest - smallest;
     }
 
     public static void main(String[] args) {
         Solution day14 = new Solution();
-        System.out.println(day14.part1());
-        System.out.println(day14.part2());
+        System.out.println(day14.part1or2(10));
+        System.out.println(day14.part1or2(40));
     }
 
     public class PairInsertion {
-        public char left;
-        public char right;
+        public Pair pair;
+        public Pair leftPair;
+        public Pair rightPair;
         public char middle;
 
         public PairInsertion(String combined, String middle) {
-            this.left = combined.charAt(0);
-            this.right = combined.charAt(1);
+            this.pair = new Pair(combined);
             this.middle = middle.charAt(0);
+            this.leftPair = new Pair(combined.substring(0, 1) + middle);
+            this.rightPair = new Pair(middle + combined.substring(1, 2));
         }
     }
 
